937d0bf23caa87a50ebf7073a01df3ece0d9ba0d
seungho <96869910+AzureSeungho@users.noreply.github.com>
2022-09-27 17:10:17 +0900

    Delete bookmanagement directory

--- bookmanagement/Readme.md
+++
@@ -1,1 +0,0 @@
-Must run BookmanagementApplication.java first in IntelliJ and type 127.0.0.1:8089/select on chrome
--- bookmanagement/src/main/java/com/seungho/book/controller/RestController.java
+++
@@ -1,9 +0,0 @@
-package com.seungho.book.controller;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
